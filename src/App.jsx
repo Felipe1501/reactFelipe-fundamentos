bc9d2a63bc5578d919ba91ccf29bb77a9b6ab8e9
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-30 11:00:46 -0300

    aula 24 fix: components card responsives

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import React from "react";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
@@ -7,31 +8,29 @@ import Card from "./components/basicos/layout/Card";
 
 export default () => (
 
-    <div id="app">
+    <div className="App">
         <h1>Fundamentos React (arrow_)</h1>
+        <div className="Cards">
+            <Card titulo="#4 - Desafio Aleatório"> 
+                <Aleatorio min={10} max={60} />
+            </Card>
 
-        <Card titulo="#4 - Desafio Aleatório"> 
-            <Aleatorio min={10} max={60} />
-        </Card>
+            <Card titulo="#3 - Fragmento"> 
+                    <Fragment />
+            </Card>
 
-        <Card titulo="#3 - Fragmento"> 
-                <Fragment />
-        </Card>
+            <Card titulo="#02 - Com Parâmetros"> 
+            <ComParametro
+                titulo="Situação Aluno"
+                aluno="Felipe Barbosa"
+                nota="9.5"
+            />
+            </Card>
 
-        <Card titulo="#02 - Com Parâmetros"> 
-        <ComParametro
-            titulo="Situação Aluno"
-            aluno="Felipe Barbosa"
-            nota="9.5"
-        />
-        </Card>
-
-        <Card titulo="#01 - Primeiro Componente"> 
-            <Primeiro />
-        </Card>
-        
-        
-
+            <Card titulo="#01 - Primeiro Componente"> 
+                <Primeiro />
+            </Card>
+        </div>
         
     </div>
 
