b073ee2122e70d0d4dd205eb82bf03c08a8dd2e9
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-30 10:15:30 -0300

    aula 22 fix: random number challenge

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragment from "./components/basicos/Fragment";
+import Aleatorio from "./components/basicos/Aleatorio";
+
 
 export default () => (
 
     <div id="app">
         <h1>Fundamentos React (arrow_)</h1>
+        <Aleatorio min={10} max={60} />
         <Fragment />
         <ComParametro
             titulo="Situação Aluno"
