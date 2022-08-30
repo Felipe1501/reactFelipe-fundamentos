87844dc1a2cae91a3d6e32b8d95e1e5fbe9fb28e
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-30 09:35:57 -0300

    aula 17 fix: component with property

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import './index.css';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Primeiro from './components/basicos/Primeiro';
-
+import ComParametro from './components/basicos/ComParametro';
 
 
 ReactDOM.render(
-    <div>
+    <div id='app'>
        <Primeiro />
+       <ComParametro 
+        titulo="Situação Aluno"
+        aluno="Felipe Barbosa"
+        nota="9.5"
+        />
+       <ComParametro 
+        titulo="Situação Aluno"
+        aluno="Thomas"
+        nota="5"
+        />
        </div>,
     document.getElementById('root')
 );
