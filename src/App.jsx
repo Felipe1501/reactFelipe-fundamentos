419e52458a2eec76428d03adbd3f3cea53eac116
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-30 10:41:11 -0300

    aula 23 fix: card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,36 @@ import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragment from "./components/basicos/Fragment";
 import Aleatorio from "./components/basicos/Aleatorio";
-
+import Card from "./components/basicos/layout/Card";
 
 export default () => (
 
     <div id="app">
         <h1>Fundamentos React (arrow_)</h1>
-        <Aleatorio min={10} max={60} />
-        <Fragment />
+
+        <Card titulo="#4 - Desafio Aleatório"> 
+            <Aleatorio min={10} max={60} />
+        </Card>
+
+        <Card titulo="#3 - Fragmento"> 
+                <Fragment />
+        </Card>
+
+        <Card titulo="#02 - Com Parâmetros"> 
         <ComParametro
             titulo="Situação Aluno"
             aluno="Felipe Barbosa"
             nota="9.5"
         />
-        <Primeiro />
+        </Card>
+
+        <Card titulo="#01 - Primeiro Componente"> 
+            <Primeiro />
+        </Card>
+        
+        
+
+        
     </div>
 
 );
