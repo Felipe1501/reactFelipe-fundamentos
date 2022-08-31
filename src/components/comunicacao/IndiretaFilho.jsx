78adb4b8bc2b6f6d0f99bbb994666547fb8c6f37
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-31 11:05:51 -0300

    aula 35 fix: stateful component

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import IndiretaPai from "./IndiretaPai";
 
 export default props =>{
     const cb = props.quandoClicar;
+    const gerarIdade = () => parseInt(Math.random() * (19)) + 40;
+    const gerarNerd = () => Math.random() > 0.5;
+    
     return(
         <div>
             <div>Filho</div>
                 <button 
-                onClick={_ =>cb('João', 58, true)
+                onClick={_ => cb('João', gerarIdade(), gerarNerd())
                 }>
                     Fornecer Informações
                     </button>
